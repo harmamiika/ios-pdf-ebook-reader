e2749c1d74542b9bd15bab5bac32c95aa3f98e7b
Miika Härmä <harmamiika@gmail.com>
2022-05-11 19:16:13 +0300

    added navigation, need to import react

--- index.js
+++ index.js
@@ -2,8 +2,18 @@
  * @format
  */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {NavigationContainer} from '@react-navigation/native';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => {
+  return (
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Root);
