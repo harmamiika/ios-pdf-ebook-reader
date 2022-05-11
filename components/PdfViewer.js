f7644f12ed182ac1f2402a11c97ac2b247ec544f
Miika Härmä <harmamiika@gmail.com>
2022-05-11 19:35:06 +0300

    navigation perhaps working??

---
+++ components/PdfViewer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function PdfViewer() {
+  return (
+    <SafeAreaView>
+      <Text>PDF</Text>
+    </SafeAreaView>
+  );
+}
--- index.js
+++ index.js
@@ -5,13 +5,20 @@
 import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
+// import PdfViewer from './components/PdfViewer';
 import {name as appName} from './app.json';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const Root = () => {
   return (
     <NavigationContainer>
-      <App />
+      <Stack.Navigator>
+        <Stack.Screen name="App" component={App} />
+        {/* <Stack.Screen name="PdfViewer" component={<PdfViewer />} /> */}
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
