9935ccdb00d7f362e0a2c2f33082526f4b9d4ab1
Miika Härmä <harmamiika@gmail.com>
2022-05-11 22:44:18 +0300

    refactor to src

--- index.js
+++ index.js
@@ -5,13 +5,13 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './App';
-import PdfViewer from './components/PdfViewer';
-import Menu from './components/Menu';
+import PdfViewer from './src/components/PdfViewer';
+import Menu from './src/components/Menu';
 import { name as appName } from './app.json';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
-import { store } from './state/store';
+import { store } from './src/state/store';
 
 const Stack = createNativeStackNavigator();
 
