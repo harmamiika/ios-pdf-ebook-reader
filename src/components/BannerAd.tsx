f552abfc8127b4efbee94f5ca018b4713f5856c2
Miika Härmä <harmamiika@gmail.com>
2022-06-11 15:09:42 +0300

    bannerad used almost prperly

--- src/components/BannerAd.tsx
+++ src/components/BannerAd.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import {
-  AppOpenAd,
-  InterstitialAd,
-  RewardedAd,
   BannerAd,
-  TestIds,
   BannerAdSize,
+  TestIds,
 } from 'react-native-google-mobile-ads';
 
 export function AdmobBannerAd() {
-  return <BannerAd size={BannerAdSize.BANNER} unitId={TestIds.BANNER} />;
+  return (
+    <View style={s.adContainer}>
+      <BannerAd size={BannerAdSize.BANNER} unitId={TestIds.BANNER} />
+    </View>
+  );
 }
+
+const s = StyleSheet.create({
+  adContainer: {
+    marginBottom: 100,
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    alignContent: 'center',
+  },
+});
---
+++ src/components/Library.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+import { RootState } from '../state';
+import { AdmobBannerAd } from './BannerAd';
+import BookListItem, {
+  screenHeight,
+  screenWidth,
+} from './book-list/BookListItem';
+
+export default function Library() {
+  const { bookList } = useSelector((state: RootState) => state.books);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        {bookList.map(book => (
+          <BookListItem book={book} key={book.id} />
+        ))}
+      </ScrollView>
+      <AdmobBannerAd />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: screenWidth,
+    height: screenHeight,
+    // backgroundColor: 'blue',
+    display: 'flex',
+    alignContent: 'flex-end',
+  },
+  scrollView: {
+    width: screenWidth,
+    height: screenHeight,
+  },
+});
