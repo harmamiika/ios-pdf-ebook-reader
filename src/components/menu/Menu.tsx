f78acced42003856902f90d3b528c3a79f1042f8
Miika Härmä <harmamiika@gmail.com>
2023-01-10 22:05:35 +0200

    change second admob ad id

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -55,7 +55,7 @@ export const Menu = ({
           <MiikaMenuItem text={item.text} onPress={item.onPress} key={index} />
         ))}
       </View>
-      <AdmobBannerAd adUnitId="ca-app-pub-8279790179515379/2242175832" />
+      <AdmobBannerAd adUnitId="ca-app-pub-8279790179515379/6306346256" />
     </SafeAreaView>
   );
 };
